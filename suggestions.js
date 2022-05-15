22fb75fa4b2317ab595e464c3341e6862489dfd0
Arthur Lovekin <arthurlovekin@gmail.com>
2022-05-15 13:10:57 -0700

    added rudimentary search box

---
+++ suggestions.js
@@ -0,0 +1,18 @@
+let suggestions = [
+    "Itching",
+    "Skin rash",
+    "Nodal skin eruptions",
+    "Dischromatic patches",
+    "Continuous Sneezing",
+    "Shivering",
+    "Chills",
+    "Watering from eyes",
+    "Stomach pain",
+    "Acidity",
+    "Ulcers on tongue",
+    "Vomiting",
+    "Cough",
+    "Chest Pain",
+];
+//Not complete; write a script to fill in 
+//or figure out a better way to represent data
