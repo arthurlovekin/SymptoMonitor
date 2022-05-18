d469eb35d6b2bedebf547105d15abb4398f1e98b
Arthur Lovekin <arthurlovekin@gmail.com>
2022-05-17 22:44:23 -0700

    Restructured File Tree

---
+++ js/learningjs/classify.js
@@ -0,0 +1,8 @@
+// Functions to classify a set of symptoms into a disease
+// Needs learningjs.js and data_util.js to be included befor this one
+
+//DO I need to include these here or is it enough to include the <script> tags in html before this one?
+var learningjs = require('./learningjs.js');
+var data_util = require("./data_util.js");
+
+var filename = 
